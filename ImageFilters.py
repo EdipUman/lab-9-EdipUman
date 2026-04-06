284f86003dbc8b5d45f3f8e5d6cf5fd63a1137b2
EdipUman <euman@unomaha.edu>
2026-04-05 23:36:46 -0500

    Completed ImageFilters.py
    Had to fork repository. Link from canvas to lab 9 wasn't working correctly for me.

--- ImageFilters.py
+++ ImageFilters.py
@@ -1,45 +1,53 @@
 # Lab 9 – Image Processing
-# Name:
-# Date:
-# Assignment:
+# Name: Edip Uman
+# Date: 4/5/2026
+# Assignment: Lab 9 
 
 from PIL import Image
 
 
 def swapGreenBlue(img):
     """Swap the green and blue values for every pixel in the image."""
-    
     pixels = img.load()
     width, height = img.size
 
     # TODO: Loop through every pixel and swap green and blue values
+    for x in range(width):
+        for y in range(height):
+            red, green, blue, alpha = pixels[x, y]
+            pixels[x, y] = (red, blue, green, alpha)
 
     img.save("swapGB.png")
 
 
 def darken(img, amount):
     """Darken the image by reducing RGB values by the given amount."""
-    
     pixels = img.load()
     width, height = img.size
 
     # TODO: Loop through every pixel and reduce RGB values by amount
     # Make sure values do not go below 0
+    for x in range(width):
+        for y in range(height):
+            red, green, blue, alpha = pixels[x, y]
+            red = max(red - amount, 0)
+            green = max(green - amount, 0)
+            blue = max(blue - amount, 0)
+            pixels[x, y] = (red, green, blue, alpha)
 
     img.save("darkImg.png")
 
 
 def bwFilter(img):
     """Example function: converts image to grayscale."""
-    
     pixels = img.load()
     width, height = img.size
 
     for x in range(width):
         for y in range(height):
-            red, green, blue = pixels[x, y]
+            red, green, blue, alpha = pixels[x, y]
             avg = (red + green + blue) // 3
-            pixels[x, y] = (avg, avg, avg)
+            pixels[x, y] = (avg, avg, avg, alpha)
 
     img.save("bwImg.png")
 
@@ -52,8 +60,8 @@ def main():
     # bwFilter(myImg)
 
     # Uncomment each function as you complete it
-    # swapGreenBlue(myImg)
-    # darken(myImg, 20)
+    swapGreenBlue(myImg)
+    darken(myImg, 20)
 
 
 if __name__ == "__main__":
